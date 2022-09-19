1b57653df224d461cb694de89a8240609d6258c3
smili08 <vvcruzpelissoni08@gmail.com>
2022-09-19 17:29:42 -0300

    commitado

---
+++ src/Scripts/tradutor.js
@@ -0,0 +1,21 @@
+
+//sa porra tradus pra portugues
+$(function() {
+    $(".br").click(function() {     
+           
+    });
+});
+
+//sa porra pra ingles
+$(function() {
+    $(".en").click(function() {
+            
+    });
+});
+
+//isso pra espanhol
+$(function() {
+    $(".es").click(function() {
+              
+     });
+});
