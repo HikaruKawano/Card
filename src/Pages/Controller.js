c7fefc8135562e553c99c308c508639a31899d89
HikaruKawano <108588173+HikaruKawano@users.noreply.github.com>
2022-09-26 20:42:39 -0300

    Parte funcional funcionando em partes

--- src/Pages/Controller.js
+++ src/Pages/Controller.js
@@ -2,7 +2,15 @@ var click = 0;
 function CardClick(){
   click = click + 1;
   console.log(click);
-}
 
+  if( click == 1 ){
+    document.getElementById('card').classList.add('CardClick');
+    document.getElementById('card').classList.remove('Card');
+  }else if( click == 2){
+    document.getElementById('card').classList.remove('CardClick');
+    document.getElementById('card').classList.add('Card');
+    click = 0;
+  }
+}
 
 
